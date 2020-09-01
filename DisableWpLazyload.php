09393cc6131b44c53e655829f1306f3c61241e21
KAMINOWEB INC <8475081+kaminoweb@users.noreply.github.com>
2020-09-01 20:13:16 +0000

    Add files via upload
    updated

---
+++ DisableWpLazyload.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Plugin Name:       Disable WordPress Lazyload
+* Plugin URI:        https://github.com/kaminoweb
+* Description:       Disable native WordPress Lazyload
+* Version:           0.1
+* Requires at least: 5.5
+* Author:            Kaminoweb Inc
+* Author URI:        https://kaminoweb.com/
+* License:           GPLv2
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+function kaminoweb_disable_lazyload( $default, $tag_name, $context ) {
+         return false;
+}
+
+add_filter('wp_lazy_loading_enabled', 'kaminoweb_disable_lazyload', 10, 3);
